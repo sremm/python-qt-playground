888762b56d60f773dbdf933e85abc368cb8549de
Sten Remmelg <sten.remmelg@gmail.com>
2021-11-07 16:15:24 +0100

    Place our simple UI into a class

--- hello.py
+++ hello.py
@@ -5,32 +5,43 @@ import sys
 from PyQt5.QtWidgets import QLineEdit, QPushButton, QVBoxLayout
 from qtpy.QtWidgets import QApplication, QLabel, QWidget
 
-# 2. Create an instance of QApplication
-app = QApplication(sys.argv)
+
+class MainWidget(QWidget):
+    def __init__(self, parent=None) -> None:
+        super().__init__(parent=parent)
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("PyQt5 App")
+        self.setGeometry(100, 100, 280, 80)
+        self.move(60, 15)
+
+        vertical_box_layout = QVBoxLayout()
+
+        helloMsg = QLabel("<h1>Hello World!</h1>")
+        helloMsg.move(60, 15)
+
+        text_box = QLineEdit()
+
+        button = QPushButton()
+        button.setText("I am a button.")
+
+        vertical_box_layout.addWidget(helloMsg)
+        vertical_box_layout.addWidget(text_box)
+        vertical_box_layout.addWidget(button)
+        self.setLayout(vertical_box_layout)
+
+        # Show your application's GUI
+        self.show()
 
 
-window = QWidget()
-window.setWindowTitle("PyQt5 App")
-window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
+def main():
+    app = QApplication(sys.argv)
 
-vertical_box_layout = QVBoxLayout()
+    main_widget = MainWidget()
+    # Run your application's event loop (or main loop)
+    sys.exit(app.exec_())
 
-helloMsg = QLabel("<h1>Hello World!</h1>")
-helloMsg.move(60, 15)
 
-text_box = QLineEdit()
-
-button = QPushButton()
-button.setText("I am a button.")
-
-vertical_box_layout.addWidget(helloMsg)
-vertical_box_layout.addWidget(text_box)
-vertical_box_layout.addWidget(button)
-window.setLayout(vertical_box_layout)
-
-# 4. Show your application's GUI
-window.show()
-
-# 5. Run your application's event loop (or main loop)
-sys.exit(app.exec_())
+if __name__ == "__main__":
+    main()
