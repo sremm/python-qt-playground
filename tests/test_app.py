7b2816417df85a1c72f3ec129fddd107cf20d939
Sten Remmelg <sten.remmelg@gmail.com>
2021-11-07 19:31:09 +0100

    add test for enetring an expression and reading the display

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
-from pycalc import PyCalcUi
+from pycalc import PyCalcCtrl, PyCalcUi
+from qtpy.QtCore import Qt
 
 
 class TestPyCalcUi:
@@ -8,3 +9,20 @@ class TestPyCalcUi:
 
         pycalcUI.show()
         pycalcUI.close()
+
+    def test_add_expression_and_read_display(self, qtbot):
+        pycalcUI = PyCalcUi()
+        qtbot.addWidget(pycalcUI)
+
+        PyCalcCtrl(view=pycalcUI)
+
+        qtbot.mouseClick(pycalcUI.buttons["1"], Qt.MouseButton.LeftButton)
+        qtbot.mouseClick(pycalcUI.buttons["+"], Qt.MouseButton.LeftButton)
+        qtbot.mouseClick(pycalcUI.buttons["2"], Qt.MouseButton.LeftButton)
+
+        display_contents = pycalcUI.displayText()
+
+        pycalcUI.show()
+        pycalcUI.close()
+
+        assert display_contents == "1+2"
